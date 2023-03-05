c9f0e15cec8710b4cfa19f5414a139d6233c51d3
Jacob Cunningham <jcun4128@gmail.com>
2023-03-05 12:19:07 -0600

    basic working connect

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,26 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './assets/styles/App.css';
 import { ensureConnected } from './bluetooth/web-bluetooth-repl/js/main';
 
 function App() {
+  const [connected, setConnected] = useState(false);
+
   const logger = (msg) => {
+    if (msg === 'Connected') {
+      setConnected(true);
+    }
+
     console.log(msg)
   }
 
-  useEffect(() => {
-    ensureConnected(logger);
-  }, []);
+  // useEffect(() => {
+  //   ensureConnected(logger);
+  // }, []);
 
   return (
     <div className="App">
-
+      <p>{connected ? "connected" : "disconnected"}</p>
+      <button type="button" onClick={() => ensureConnected(logger)}>Connect</button>
     </div>
   );
 }

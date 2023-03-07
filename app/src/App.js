2e136cf0bb03c0b52e3b46e636ccd4614a5fd9ed
Jacob Cunningham <jcun4128@gmail.com>
2023-03-06 23:20:07 -0600

    nasty code

--- app/src/App.js
+++ app/src/App.js
@@ -2,68 +2,107 @@ import { useState, useEffect } from 'react';
 import './assets/styles/App.css';
 import { ensureConnected } from './bluetooth/js/main';
 import { replRawMode, replSend } from './bluetooth/js/repl';
-import { getHnTopArticleComments} from './hacker-news/hn';
 
 function App() {
   const [connected, setConnected] = useState(false);
-  const [hnArticleData, setHnArticleData] = useState(false); // type mixing
-
-  const getHn = async () => {
-    const hnData = await getHnTopArticleComments();
-    setHnArticleData(hnData);
-  }
-
-  const displayHnArticleComment = (articleComments) => {
-    console.log('called');
-    return new Promise(resolve => {
-      const articleIds = Object.keys(articleComments);
-      
-      if (articleIds.length) {
-        const body = articleComments[articleIds[0]];
-
-        setTimeout(async () => {
-          let replCmd = 'import display;'
-  
-          replCmd += `display.text("${body.title.substring(0, 26)}", 0, 0, 0xffffff);`;
-          replCmd += `display.text("${body.comment.substring(0, 26)}", 0, 50, 0xffffff);`;
-          replCmd += 'display.show();'
-
-          console.log('send', replCmd);
-  
-          await replSend(replCmd);
-          delete articleComments[articleIds[0]];
-          displayHnArticleComment(articleComments);
-        }, 5000);
-      } else {
-        resolve(""); // done
-      }
-    });
-  }
+  const [running, setRunning] = useState(false);
 
   // send to monocle display
-  const displayHnArticleComments = async (hnArticleComments) => {
+  const sendToMonocle = async (replStr) => {
+    setRunning(true);
     await replRawMode(true);
-    await displayHnArticleComment(hnArticleComments);
+    await replSend(replStr);
+    setRunning(false);
+  }
+
+  const runCmd = (id) => {
+    const snippet = document.getElementById(id);
+
+    let cmdStr = '';
+
+    snippet.querySelectorAll('p').forEach(line => {
+      cmdStr += line.innerText + ';';
+    });
+
+    sendToMonocle(cmdStr);
   }
 
   const logger = async (msg) => {
     if (msg === 'Connected') {
       setConnected(true);
     }
-
-    getHn();
   }
-
-  useEffect(() => {
-    if (Object.keys(hnArticleData).length) {
-      displayHnArticleComments(hnArticleData);
-    }
-  }, [hnArticleData]);
 
   return (
     <div className="App">
-      <p>{connected ? "connected" : "disconnected"}</p>
-      <button type="button" onClick={() => ensureConnected(logger)}>Connect</button>
+      <span className="connect">
+        <p>{connected ? "connected" : "disconnected"}</p>
+        <button type="button" onClick={() => ensureConnected(logger)}>Connect</button>
+      </span>
+      <div className="container">
+        <div className="snippets">
+          <div className="snippet">
+            <span>
+              <button
+                type="button"
+                className="run"
+                disabled={running}
+                onClick={() => runCmd('white')}
+              >run</button>
+            </span>
+            <div className="body" id="white">
+              <p>import display</p>
+              <p>display.text("White text line", 0, 0, 0xffffff)</p>
+              <p>display.show()</p>
+            </div>
+          </div>
+          <div className="snippet">
+            <span>
+              <button
+                type="button"
+                className="run"
+                disabled={running}
+                onClick={() => runCmd('green')}
+              >run</button>
+            </span>
+            <div className="body" id="green">
+              <p>import display</p>
+              <p>display.text("Green text line", 0, 0, 0x00ff00)</p>
+              <p>display.show()</p>
+            </div>
+          </div>
+          <div className="snippet">
+            <span>
+              <button
+                type="button"
+                className="run"
+                disabled={running}
+                onClick={() => runCmd('blue')}
+              >run</button>
+            </span>
+            <div className="body" id="blue">
+              <p>import display</p>
+              <p>display.text("Blue text line", 0, 0, 0x0000ff)</p>
+              <p>display.show()</p>
+            </div>
+          </div>
+          <div className="snippet">
+            <span>
+              <button
+                type="button"
+                className="run"
+                disabled={running}
+                onClick={() => runCmd('red')}
+              >run</button>
+            </span>
+            <div className="body" id="red">
+              <p>import display</p>
+              <p>display.text("Red text line", 0, 0, 0xff0000)</p>
+              <p>display.show()</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

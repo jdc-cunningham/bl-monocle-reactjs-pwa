a434156542db4ee338cc47407b5efbd717d669b4
Jacob Cunningham <jcun4128@gmail.com>
2023-03-18 13:38:31 -0500

    basic single repl supports functions

--- app/src/App.js
+++ app/src/App.js
@@ -6,6 +6,11 @@ import { replRawMode, replSend } from './bluetooth/js/repl';
 function App() {
   const [connected, setConnected] = useState(false);
   const [running, setRunning] = useState(false);
+  const [snippet, setSnippet] = useState([
+    'import display',
+    'display.text("White text line", 0, 0, 0xffffff)',
+    'display.show()',
+  ].join('\n'));
 
   // send to monocle display
   const sendToMonocle = async (replStr) => {
@@ -15,16 +20,18 @@ function App() {
     setRunning(false);
   }
 
+  const prepSend = (snippet) => {
+
+    // https://github.com/siliconwitchery/web-bluetooth-repl/blob/b13ade8c1aa9754e4a2ad917c2d227705c02ef7f/js/repl.js#L269
+    let string = '';
+    string = snippet.replaceAll('\r\n', '\r');
+    string = string.replaceAll('\n', '\r');
+  
+    sendToMonocle(string);
+  }
+
   const runCmd = (id) => {
-    const snippet = document.getElementById(id);
-
-    let cmdStr = '';
-
-    snippet.querySelectorAll('p').forEach(line => {
-      cmdStr += line.innerText + ';';
-    });
-
-    sendToMonocle(cmdStr);
+    prepSend(snippet);
   }
 
   const logger = async (msg) => {
@@ -50,56 +57,7 @@ function App() {
                 onClick={() => runCmd('white')}
               >run</button>
             </span>
-            <div className="body" id="white">
-              <p>import display</p>
-              <p>display.text("White text line", 0, 0, 0xffffff)</p>
-              <p>display.show()</p>
-            </div>
-          </div>
-          <div className="snippet">
-            <span>
-              <button
-                type="button"
-                className="run"
-                disabled={running}
-                onClick={() => runCmd('green')}
-              >run</button>
-            </span>
-            <div className="body" id="green">
-              <p>import display</p>
-              <p>display.text("Green text line", 0, 0, 0x00ff00)</p>
-              <p>display.show()</p>
-            </div>
-          </div>
-          <div className="snippet">
-            <span>
-              <button
-                type="button"
-                className="run"
-                disabled={running}
-                onClick={() => runCmd('blue')}
-              >run</button>
-            </span>
-            <div className="body" id="blue">
-              <p>import display</p>
-              <p>display.text("Blue text line", 0, 0, 0x0000ff)</p>
-              <p>display.show()</p>
-            </div>
-          </div>
-          <div className="snippet">
-            <span>
-              <button
-                type="button"
-                className="run"
-                disabled={running}
-                onClick={() => runCmd('red')}
-              >run</button>
-            </span>
-            <div className="body" id="red">
-              <p>import display</p>
-              <p>display.text("Red text line", 0, 0, 0xff0000)</p>
-              <p>display.show()</p>
-            </div>
+            <textarea className="body" id="white" value={snippet} onChange={(e) => setSnippet(e.target.value)}></textarea>
           </div>
         </div>
       </div>

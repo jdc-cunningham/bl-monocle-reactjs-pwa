38c9fc93ae6b9418d83b46e89cf8a96a4bce4431
Jacob Cunningham <jcun4128@gmail.com>
2023-03-19 13:09:58 -0500

    envelope icon

--- python-snippets/envelope.py
+++ python-snippets/envelope.py
@@ -4,21 +4,20 @@ def draw_h_borders():
   display.hline(0, 0, 640, 0xffffff)
   display.hline(0, 392, 640, 0xffffff)
 
-# 10 objects
-def draw_envelope(top_left_x, top_left_y, scale, color, has_message):
-  display.line(50, 50, 50, 110, color)
-  display.line(50, 50, 150, 50, color)
-  display.line(50, 50, 100, 80, color)
-  display.line(150, 50, 100, 80, color)
-  display.line(150, 50, 150, 110, color)
-  display.line(50, 110, 150, 110, color)
-  display.line(50, 110, 79, 76, color)
-  display.line(150, 110, 119, 76, color)
+def draw_envelope(top_left_x = 50, top_left_y = 50, scale = 1, color = 0xffffff, has_message = False):
+  display.line(top_left_x, top_left_y, top_left_x, top_left_y + 60, color)
+  display.line(top_left_x, top_left_y, top_left_x + 100, top_left_y, color)
+  display.line(top_left_x, top_left_y, top_left_x + 50, top_left_y + 30, color)
+  display.line(top_left_x + 100, top_left_y, top_left_x + 50, top_left_y + 30, color)
+  display.line(top_left_x + 100, top_left_y, top_left_x + 100, top_left_y + 60, color)
+  display.line(top_left_x, top_left_y + 60, top_left_x + 100, top_left_y + 60, color)
+  display.line(top_left_x, top_left_y + 60, top_left_x + 29, top_left_y + 26, color)
+  display.line(top_left_x + 100, top_left_y + 60, top_left_x + 69, top_left_y + 26, color)
 
   if (has_message):
-    display.hline(130, 40, 25, 0xff0000)
-    display.hline(130, 44, 25, 0xff0000)
+    display.hline(top_left_x + 80, top_left_y - 10, 25, 0xff0000)
+    display.hline(top_left_x + 80, top_left_y - 6, 25, 0xff0000)
 
 draw_h_borders()
-draw_envelope(50, 50, 1, 0xffffff, True)
+draw_envelope(100, 200, 1, 0xffffff, True)
 display.show()
